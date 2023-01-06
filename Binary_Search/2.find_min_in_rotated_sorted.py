77f434b274da9007ec8a1ebb3bfb98e16b026d68
Bry <xiaocraze@gmail.com>
2023-01-06 14:51:50 +0800

    LL + binary search questions

---
+++ Binary_Search/2.find_min_in_rotated_sorted.py
@@ -0,0 +1,23 @@
+def findMin(nums):
+    l = 0 
+    r = len(nums) - 1
+    res = nums[0]
+
+    while l <=r:
+        if nums[l] < nums[r]:
+            res = min(res, nums[l])
+            break
+        
+        m = (l + r )// 2
+        res = min(res, nums[m])
+        if nums[m] >= nums[l]:
+            l = m + 1
+        else:
+            r = m - 1
+    return res
+
+
+
+
+
+print(findMin([3,1,2]))
